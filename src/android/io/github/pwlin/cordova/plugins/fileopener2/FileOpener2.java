c2974593a88d2861ee734f8657e301608f16cec8
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-07-08 01:35:24 +0800

    Update FileOpener2.java

--- src/android/io/github/pwlin/cordova/plugins/fileopener2/FileOpener2.java
+++ src/android/io/github/pwlin/cordova/plugins/fileopener2/FileOpener2.java
@@ -70,6 +70,19 @@ public class FileOpener2 extends CordovaPlugin {
 			}
 			this._open(fileUrl, contentType, openWithDefault, aPackage, callbackContext);
 		}
+		else if (action.equals("openApp")) {
+			String fileUrl = args.getString(0);
+			String contentType = args.getString(1);
+			String aPackage = args.getString(2);
+			if (aPackage.equals("")) {
+				aPackage = null;
+			}
+			Boolean openWithDefault = true;
+			if(args.length() > 3){
+				openWithDefault = args.getBoolean(3);
+			}
+			this._open(fileUrl, contentType, openWithDefault, aPackage, callbackContext);
+		}
 		else if (action.equals("uninstall")) {
 			this._uninstall(args.getString(0), callbackContext);
 		}
