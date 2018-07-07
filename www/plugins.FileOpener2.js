b3a351d5a4443c5eacfa63936a623d3eae87e8df
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-07-08 00:38:41 +0800

    Update plugins.FileOpener2.js

--- www/plugins.FileOpener2.js
+++ www/plugins.FileOpener2.js
@@ -26,14 +26,20 @@ var exec = require('cordova/exec');
 
 function FileOpener2() {}
 
-FileOpener2.prototype.open = function (fileName, contentType, packageName, callbackContext) {
+FileOpener2.prototype.open = function (fileName, contentType, callbackContext) {
+    callbackContext = callbackContext || {};
+    exec(callbackContext.success || null, callbackContext.error || null, 'FileOpener2', 'open', [fileName, contentType]);
+};
+
+FileOpener2.prototype.openApp = function (fileName, contentType, packageName, callbackContext) {
     if (typeof(packageName) === "object") {
         callbackContext = packageName;
         packageName = "";
     }
     callbackContext = callbackContext || {};
-    exec(callbackContext.success || null, callbackContext.error || null, 'FileOpener2', 'open', [fileName, contentType, packageName]);
+    exec(callbackContext.success || null, callbackContext.error || null, 'FileOpener2', 'openApp', [fileName, contentType, packageName]);
 };
+
 
 FileOpener2.prototype.showOpenWithDialog = function (fileName, contentType, callbackContext) {
     callbackContext = callbackContext || {};
